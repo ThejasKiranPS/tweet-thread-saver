f1f67eaeef1e27c6f3f8863cfc80a7eaa6d06736
Amal Paul <amalpaul54111@gmail.com>
2021-05-21 15:43:02 +0530

    Now logged in user can only see the tweets he has mentioned

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,11 +5,15 @@ from .models import Thread
 # Create your views here.
 def dashboard(request):
     
-    threads = Thread.objects.all()
+    current_user = request.user.id
+    threads = Thread.objects.filter(username=current_user)
     return render(request=request, template_name="dashboard.html", context={'threads': threads})
 
 
 def refresh(request):
-    thread_fetch.main("thejaskiranps")
+    
+    # this is where we will run the script to add new mentions to our database 
+    # and store them
+
 
     return redirect('/dashboard')

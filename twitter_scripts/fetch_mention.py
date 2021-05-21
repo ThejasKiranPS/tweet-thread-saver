177c98f3f8a4c9d7230435749705f681393a3d73
Amal Paul <amalpaul54111@gmail.com>
2021-05-22 01:15:36 +0530

    Edited dashboard.html to include thread author name and twitterid

--- twitter_scripts/fetch_mention.py
+++ twitter_scripts/fetch_mention.py
@@ -1,13 +1,13 @@
 import requests
 import json
 
-# from twitter_scripts import secrets
-# from twitter_scripts import urls
-# from twitter_scripts import Write
+from twitter_scripts import secrets
+from twitter_scripts import urls
+from twitter_scripts import Write
 
-import secrets
-import urls
-import Write
+# import secrets
+# import urls
+# import Write
 
 def auth():
     return secrets.bearer_key
